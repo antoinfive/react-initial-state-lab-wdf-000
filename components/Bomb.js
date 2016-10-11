ca01b45734787cb4fe1e9f2b4496198a6d554051
antoinfive <antoinfive@gmail.com>
2016-10-11 18:07:09 -0400

    bombs-a-tickin

---
+++ components/Bomb.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Bomb extends React.Component { 
+  constructor(props) {
+    super();
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+  
+  render() {
+    const bombMessage = this.state.secondsLeft === 0 ?'Boom!':`${this.state.secondsLeft} seconds left before I go boom!`
+    return (
+      <div>
+        {bombMessage}
+      </div>
+    )
+  }
+}
+
+module.exports = Bomb
